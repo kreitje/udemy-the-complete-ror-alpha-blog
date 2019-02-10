16b9384e714ec76de0b3227a5a0e7132c88f58bb
Jeff Kreitner <jeff@hitmyserver.com>
2019-02-10 10:46:26 -0500

    Refactor to stop calling Article.find(params[:id]) multiple times

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -9,7 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
     @article = Article.new(article_params)
 
     if @article.save
@@ -18,32 +19,24 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-    
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
-
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
     end
-
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     flash[:notice] = "#{@article.title} was removed"
@@ -52,6 +45,10 @@ class ArticlesController < ApplicationController
 
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
