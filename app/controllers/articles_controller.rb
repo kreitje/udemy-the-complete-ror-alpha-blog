34703c40a3ef92dd86b0569a77a19593a675b8a3
Jeff Kreitner <jeff@hitmyserver.com>
2019-02-10 14:35:17 -0500

    Styled error messages and flash notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was saved."
+      flash[:success] = "Article was saved."
       redirect_to article_path(@article)
     else
       render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
 
-    flash[:notice] = "#{@article.title} was removed"
+    flash[:success] = "#{@article.title} was removed"
     redirect_to articles_path
   end
 
