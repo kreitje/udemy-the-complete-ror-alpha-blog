3081ca737cb08dae3151d317c0414fb3dc50c56f
Jeff Kreitner <jeff@hitmyserver.com>
2019-02-23 23:11:19 -0500

    Restrictions based on if you are the owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -46,5 +46,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You can only edit your profile"
+      redirect_to users_path
+    end
   end
 end
